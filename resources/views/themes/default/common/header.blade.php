77ad1601f087779c4a4b26d686eeb594e0d2b2c8
anilcyberlink <anilcyberlink@gmail.com>
2026-02-25 13:09:26 +0545

    Tailwind cdn removed

--- resources/views/themes/default/common/header.blade.php
+++ resources/views/themes/default/common/header.blade.php
@@ -17,6 +17,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <!-- Flowbite & Tailwind CDN -->
     <link href="{{ asset('theme-assets/css/app.css') }}" rel="stylesheet" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('theme-assets/assets/favicon/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('theme-assets/assets/favicon/favicon-32x32.png') }}">
